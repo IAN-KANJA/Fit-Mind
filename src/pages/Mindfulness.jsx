4f93cb0c1383d6acb4aa5540a3aca9f8fee4dfbd
iannjau <lantanacreations@gmail.com>
2024-07-10 11:39:04 +0300

    add

--- src/pages/Mindfulness.jsx
+++ src/pages/Mindfulness.jsx
@@ -4,4 +4,17 @@ function MindExercise(){
   const [name, setName] = useState('')
   const [description, setDescription] = useState('');
   const [difficulty , setDifficulty] = useState('');
+  const [duration, setDuration] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const MindExerciseData = {
+      name: name,
+      description: description,
+      difficulty: difficulty,
+      duration: duration,
+
+    }
+  }
 }
