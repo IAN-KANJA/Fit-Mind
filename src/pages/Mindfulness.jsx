f8a9fd4efd28f5ddae5b079f06565eb9894fc065
iannjau <lantanacreations@gmail.com>
2024-07-10 11:11:30 +0300

    add

--- src/pages/Mindfulness.jsx
+++ src/pages/Mindfulness.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+from flask import Flask ,request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-const MindfulnessSessionsPage = () => {
 
-};
 
 export default MindfulnessSessionsPage;
